ad733fd402c13b3edd30328578624d3631bf86c6
lolluslollus <lolluslollus@hotmail.com>
2016-01-28 17:33:58 +0100

    new cts unit tests

---
+++ UnitTestProject1/LongRunner1.cs
@@ -0,0 +1,76 @@
+﻿using LolloGPS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+	public class LongRunner1 : OpenableObservableData
+	{
+		private int _howManyDelays = -1;
+		public int HowManyDelays { get { return _howManyDelays; } }
+		public async Task ManySeconds()
+		{
+			_howManyDelays = 0;
+			if (CancToken.IsCancellationRequested) return;
+			await Task.Delay(2000);
+
+			_howManyDelays ++;
+			if (CancToken.IsCancellationRequested) return;
+			await Task.Delay(2000);
+
+			_howManyDelays ++;
+			if (CancToken.IsCancellationRequested) return;
+			await Task.Delay(2000);
+
+			_howManyDelays++;
+			if (CancToken.IsCancellationRequested) return;
+			await Task.Delay(2000);
+
+			_howManyDelays++;
+			if (CancToken.IsCancellationRequested) return;
+			await Task.Delay(2000);
+		}
+
+		public void Cancel()
+		{
+			Cts.Cancel();
+		}
+	}
+	public class LongRunner2 : OpenableObservableData
+	{
+		private int _howManyDelays = -1;
+		public int HowManyDelays { get { return _howManyDelays; } }
+		public Task ManySecondsWhenOpen()
+		{
+			return RunFunctionIfOpenAsyncT(async delegate
+			{
+				_howManyDelays = 0;
+				if (CancToken.IsCancellationRequested) return;
+				await Task.Delay(2000);
+
+				_howManyDelays++;
+				if (CancToken.IsCancellationRequested) return;
+				await Task.Delay(2000);
+
+				_howManyDelays++;
+				if (CancToken.IsCancellationRequested) return;
+				await Task.Delay(2000);
+
+				_howManyDelays++;
+				if (CancToken.IsCancellationRequested) return;
+				await Task.Delay(2000);
+
+				_howManyDelays++;
+				if (CancToken.IsCancellationRequested) return;
+				await Task.Delay(2000);
+			});
+		}
+		public void Cancel()
+		{
+			Cts.Cancel();
+		}
+	}
+}
