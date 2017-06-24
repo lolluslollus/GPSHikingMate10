2e7d845411e052d2b5c9251219e8587d95d92203
lorenzo felloni <lollus@hotmail.co.uk>
2017-06-24 20:11:08 +0200

    Water level bubble

--- GPSHikingMate10/Controlz/CompassControl.xaml.cs
+++ GPSHikingMate10/Controlz/CompassControl.xaml.cs
@@ -12,10 +12,16 @@ namespace LolloGPS.Controlz
 {
     public sealed partial class CompassControl : Utilz.Controlz.OpenableObservableControl
     {
-        private const int MIN_MIN_REPORT_INTERVAL = 16;
-        private Compass _compass;
-        private int _desiredReportInterval;
-        private DispatcherTimerPlus _dispatcherTimer;
+        private const int MIN_MIN_COMPASS_REPORT_INTERVAL = 16;
+        private const int MIN_MIN_INCLINOMETER_REPORT_INTERVAL = 16;
+        private const int MIN_USEFUL_COMPASS_REPORT_INTERVAL = 100;
+        private const int MIN_USEFUL_INCLINOMETER_REPORT_INTERVAL = 100;
+        private readonly Compass _compass;
+        private readonly Inclinometer _inclinometer;
+        private readonly int _desiredCompassReportInterval;
+        private readonly int _desiredInclinometerReportInterval;
+        private DispatcherTimerPlus _dispatcherTimer4Compass;
+        private DispatcherTimerPlus _dispatcherTimer4Inclinometer;
 
         private static readonly BitmapImage BadImage = new BitmapImage(new Uri("ms-appx:///Assets/compass-outer-bad-90.png"));
         private static readonly BitmapImage MediumImage = new BitmapImage(new Uri("ms-appx:///Assets/compass-outer-medium-90.png"));
@@ -25,6 +31,13 @@ namespace LolloGPS.Controlz
         {
             this.InitializeComponent();
             _compass = Compass.GetDefault();
+            _inclinometer = Inclinometer.GetDefault();
+            // Select a report interval that is both suitable for the purposes of the app and supported by the sensor.
+            // This value will be used later to activate the sensor.
+            int minCompassReportInterval = (int)_compass.MinimumReportInterval;
+            _desiredCompassReportInterval = minCompassReportInterval > MIN_USEFUL_COMPASS_REPORT_INTERVAL ? minCompassReportInterval : MIN_USEFUL_COMPASS_REPORT_INTERVAL;
+            int minInclinometerReportInterval = (int)_inclinometer.MinimumReportInterval;
+            _desiredInclinometerReportInterval = minInclinometerReportInterval > MIN_USEFUL_INCLINOMETER_REPORT_INTERVAL ? minInclinometerReportInterval : MIN_USEFUL_INCLINOMETER_REPORT_INTERVAL;
         }
 
         protected override Task OpenMayOverrideAsync(object args = null)
@@ -32,15 +45,11 @@ namespace LolloGPS.Controlz
             if (_compass != null)
             {
                 LayoutRoot.Visibility = Visibility.Visible;
-                // Select a report interval that is both suitable for the purposes of the app and supported by the sensor.
-                // This value will be used later to activate the sensor.
-                int minReportInterval = (int)_compass.MinimumReportInterval;
-                _desiredReportInterval = minReportInterval > MIN_MIN_REPORT_INTERVAL ? minReportInterval : MIN_MIN_REPORT_INTERVAL;
 
-                DisplayCurrentReading();
-                _dispatcherTimer?.Dispose();
-                _dispatcherTimer = new DispatcherTimerPlus(DisplayCurrentReading, _desiredReportInterval);
-                _dispatcherTimer.Start();
+                DisplayCurrentCompassReading();
+                _dispatcherTimer4Compass?.Dispose();
+                _dispatcherTimer4Compass = new DispatcherTimerPlus(DisplayCurrentCompassReading, _desiredCompassReportInterval);
+                _dispatcherTimer4Compass.Start();
 
                 _compass.ReadingChanged += OnCompass_ReadingChanged;
             }
@@ -48,22 +57,44 @@ namespace LolloGPS.Controlz
             {
                 LayoutRoot.Visibility = Visibility.Collapsed;
             }
+
+            if (_inclinometer != null)
+            {
+                DisplayCurrentInclinometerReading();
+                _dispatcherTimer4Inclinometer?.Dispose();
+                _dispatcherTimer4Inclinometer = new DispatcherTimerPlus(DisplayCurrentInclinometerReading, _desiredInclinometerReportInterval);
+                _dispatcherTimer4Inclinometer.Start();
+
+                _inclinometer.ReadingChanged += OnInclinometer_ReadingChanged;
+            }
+            else
+            {
+                InclinometerImage.Visibility = Visibility.Collapsed;
+            }
             return Task.CompletedTask;
         }
 
         protected override Task CloseMayOverrideAsync(object args = null)
         {
-            _dispatcherTimer?.Dispose();
+            _dispatcherTimer4Compass?.Dispose();
+            _dispatcherTimer4Compass = null;
+            _dispatcherTimer4Inclinometer?.Dispose();
+            _dispatcherTimer4Inclinometer = null;
             var compass = _compass; if (compass != null) compass.ReadingChanged -= OnCompass_ReadingChanged;
+            var incl = _inclinometer; if (incl != null) incl.ReadingChanged -= OnInclinometer_ReadingChanged;
             return Task.CompletedTask;
         }
 
         private void OnCompass_ReadingChanged(Compass sender, CompassReadingChangedEventArgs args)
         {
-            DisplayCurrentReading();
+            DisplayCurrentCompassReading();
+        }
+        private void OnInclinometer_ReadingChanged(Inclinometer sender, InclinometerReadingChangedEventArgs args)
+        {
+            DisplayCurrentInclinometerReading();
         }
 
-        private void DisplayCurrentReading()
+        private void DisplayCurrentCompassReading()
         {
             var compass = _compass;
             if (compass == null) return;
@@ -73,41 +104,40 @@ namespace LolloGPS.Controlz
                 var reading = compass.GetCurrentReading();
 
                 if (reading == null || reading.HeadingTrueNorth == null)
-                    SetHeadingNone();
+                    SetHeadingNoneAsync();
                 else
                 {
-                    SetHeading((double)reading.HeadingTrueNorth, reading.HeadingAccuracy);
-                    //switch (reading.HeadingAccuracy)
-                    //{
-                    //    case MagnetometerAccuracy.Unknown:
-                    //        SetHeadingNone();
-                    //        break;
-                    //    case MagnetometerAccuracy.Unreliable:
-                    //        SetHeadingNone();
-                    //        break;
-                    //    case MagnetometerAccuracy.Approximate:
-                    //        SetHeading((double)reading.HeadingTrueNorth, true);
-                    //        break;
-                    //    case MagnetometerAccuracy.High:
-                    //        SetHeading((double)reading.HeadingTrueNorth, false);
-                    //        break;
-                    //    default:
-                    //        SetHeadingNone();
-                    //        break;
-                    //}
+                    SetHeadingAsync((double)reading.HeadingTrueNorth, reading.HeadingAccuracy);
                 }
             }
             catch { }
         }
-        private async void SetHeadingNone()
+        private void DisplayCurrentInclinometerReading()
         {
-            await RunInUiThreadAsync(() => { Heading.Rotation = 0.0; LayoutRoot.Visibility = Visibility.Collapsed; }).ConfigureAwait(false);
+            var inclinometer = _inclinometer;
+            if (inclinometer == null) return;
+
+            try
+            {
+                // pitch is negative if the top of the screen is lower than the bottom
+                // roll is negative if the left side of the screen is lower than the right
+                // values are in degrees
+                var reading = inclinometer.GetCurrentReading();
+                if (reading == null) SetRollPitchNoneAsync();
+                else SetRollPitchAsync(reading.PitchDegrees, reading.RollDegrees);
+            }
+            catch { }
         }
-        //private async void SetHeading(double newValue, bool isUncertain)
-        //{
-        //	await RunInUiThreadAsync(() => { Heading.Rotation = -newValue; LayoutRoot.Visibility = Visibility.Visible; LayoutRoot.Opacity = isUncertain ? .5 : 1.0; }).ConfigureAwait(false);
-        //}
-        private async void SetHeading(double newValue, MagnetometerAccuracy accuracy)
+
+        private Task SetHeadingNoneAsync()
+        {
+            return RunInUiThreadAsync(() =>
+            {
+                Heading.Rotation = 0.0;
+                LayoutRoot.Visibility = Visibility.Collapsed;
+            });
+        }
+        private Task SetHeadingAsync(double newValue, MagnetometerAccuracy accuracy)
         {
             BitmapImage source = null;
             switch (accuracy)
@@ -122,7 +152,35 @@ namespace LolloGPS.Controlz
                     source = BadImage;
                     break;
             }
-            await RunInUiThreadAsync(() => { Heading.Rotation = -newValue; LayoutRoot.Visibility = Visibility.Visible; MyImage.Source = source; }).ConfigureAwait(false);
+            return RunInUiThreadAsync(() =>
+            {
+                Heading.Rotation = -newValue;
+                LayoutRoot.Visibility = Visibility.Visible;
+                CompassImage.Source = source;
+            });
+        }
+
+        private Task SetRollPitchNoneAsync()
+        {
+            return RunInUiThreadAsync(() =>
+            {
+                RollPitch.TranslateX = 0.0;
+                RollPitch.TranslateY = 0.0;
+                InclinometerImage.Visibility = Visibility.Collapsed;
+            });
+        }
+        private Task SetRollPitchAsync(double pitchDegrees, double rollDegrees)
+        {
+            return RunInUiThreadAsync(() =>
+            {
+                try
+                {
+                    RollPitch.TranslateX = -rollDegrees / 180.0 * 30.0;
+                    RollPitch.TranslateY = -pitchDegrees / 180.0 * 30.0;
+                    InclinometerImage.Visibility = Visibility.Visible;
+                }
+                catch { }
+            });
         }
     }
 }
